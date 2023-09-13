b0438decbc94849322c881d234c3bab2ccebb4f9
Nathanael Abel <75358980+nathanaelabel@users.noreply.github.com>
2023-09-13 11:31:18 +0700

    feat: update n' pass url via .env

--- moodle/lib/classes/navigation/output/primary.php
+++ moodle/lib/classes/navigation/output/primary.php
@@ -104,11 +104,19 @@ class primary implements renderable, templatable {
         $coursecontext = \context_course::instance($COURSE->id);
         $systemcontext = \context_system::instance();
 
+        // if(isloggedin()) {
+        //     if (has_capability('moodle/role:manage', $coursecontext)) {
+        //         $CFG->custommenuitems = "Back|http://localhost:3000/courses/moodle";
+        //     } else {
+        //         $CFG->custommenuitems = "Back|http://localhost:3000/profile";
+        //     }
+        // }
+
         if(isloggedin()) {
             if (has_capability('moodle/role:manage', $coursecontext)) {
-                $CFG->custommenuitems = "Back|http://localhost:3000/courses/moodle";
+                $CFG->custommenuitems = "Back|" + getenv('METRODATA_ACADEMY_COURSE_URL');
             } else {
-                $CFG->custommenuitems = "Back|http://localhost:3000/profile";
+                $CFG->custommenuitems = "Back|" + getenv('METRODATA_ACADEMY_PROFILE_URL');
             }
         }
 
